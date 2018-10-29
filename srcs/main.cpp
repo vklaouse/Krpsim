7876a37f672d232733519ece6bc81f9f1b4bed7e
Claudio MUTTI <cmutti@e3r4p3.42.fr>
2018-10-29 18:49:07 +0100

    lifeTime done

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,37 @@
 #include "Lexer.class.hpp"
 #include "Parser.class.hpp"
 
+int verifyLifeTime(std::string sLifeTime) {
+	double tmpLifetime;
+
+	for (size_t i = 0; sLifeTime.size() > i; i++) {
+		if (isdigit(sLifeTime[i]) == false)
+			return -1;
+	}
+	try {
+		tmpLifetime = std::stod(sLifeTime);
+	}
+	catch (std::exception & e) {
+		return -1;
+	}
+	if (tmpLifetime >= std::numeric_limits<int>::max() || tmpLifetime <= 0)
+		return -1;
+	return static_cast<int>(tmpLifetime);
+}
+
 void usage() {
-	std::cout << "./krpsim file_path" << std::endl;
+	std::cerr << "./krpsim file_path lifetime" << std::endl;
 }
 
 int main(int ac, char **av) {
-	if (ac == 2)
+	if (ac == 3)
 	{
+		int lifeTime = verifyLifeTime(av[2]);
+		if (lifeTime == -1) {
+			std::cerr << "Lifetime must be positive int" << std::endl;
+			usage();
+			return 0;
+		}
 		Lexer tokenizer = Lexer(av[1]);
 		std::vector<std::string> &errors = tokenizer.getErrors();
 		std::vector<Token> &tokens = tokenizer.getTokens();
@@ -41,8 +65,11 @@ int main(int ac, char **av) {
 		for (size_t i = 0; i < parser.getGoal().size(); i++) {
 			std::cout << "Name : " << parser.getGoal()[i].name << " -> Optimize time : " << parser.getGoal()[i].optimizeTime << std::endl;
 		}
+		// parser.runSimlation();
 	}
-	else
+	else {
+		std::cerr << "Wrong number of parameters" << std::endl;
 		usage();
+	}
 	return 0;
 }
